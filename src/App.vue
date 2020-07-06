d0dad455ec219ff1e9d2cf9f19ceca1a22a279e0
frozenwhale <1614395133@qq.com>
2020-07-06 17:24:47 +0800

    add face select module

--- src/App.vue
+++ src/App.vue
@@ -3,76 +3,27 @@
     <v-navigation-drawer
       v-model="drawer"
       :clipped="$vuetify.breakpoint.lgAndUp"
+      :mini-variant.sync="mini"
       app
     >
       <v-list dense>
-        <template v-for="item in items">
-          <v-row
-            v-if="item.heading"
-            :key="item.heading"
-            align="center"
-          >
-            <v-col cols="6">
-              <v-subheader v-if="item.heading">
-                {{ item.heading }}
-              </v-subheader>
-            </v-col>
-            <v-col
-              cols="6"
-              class="text-center"
-            >
-              <a
-                href="#!"
-                class="body-2 black--text"
-              >EDIT</a>
-            </v-col>
-          </v-row>
-          <v-list-group
-            v-else-if="item.children"
-            :key="item.text"
-            v-model="item.model"
-            :prepend-icon="item.model ? item.icon : item['icon-alt']"
-            append-icon=""
-          >
-            <template v-slot:activator>
-              <v-list-item-content>
-                <v-list-item-title>
-                  {{ item.text }}
-                </v-list-item-title>
-              </v-list-item-content>
-            </template>
-            <v-list-item
-              v-for="(child, i) in item.children"
-              :key="i"
-              link
-            >
-              <v-list-item-action v-if="child.icon">
-                <v-icon>{{ child.icon }}</v-icon>
-              </v-list-item-action>
-              <v-list-item-content>
-                <v-list-item-title>
-                  {{ child.text }}
-                </v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list-group>
-          <v-list-item
-            v-else
-            :key="item.text"
-            link
-          >
-            <v-list-item-action>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>
-                {{ item.text }}
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </template>
+        <v-list-item
+          v-for="(item, index) in items"
+          :key="item.text"
+          link
+          @click="test(index)"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
       </v-list>
     </v-navigation-drawer>
+
     <!-- app -bar -->
     <v-app-bar
       :clipped-left="$vuetify.breakpoint.lgAndUp"
@@ -80,7 +31,7 @@
       color="blue darken-3"
       dark
     >
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="mini = !mini"></v-app-bar-nav-icon>
       <v-toolbar-title
         style="width: 300px"
         class="ml-0 pl-4"
@@ -194,6 +145,7 @@
         </v-avatar>
       </v-btn>
     </v-app-bar>
+
     <div class="content-app">
       <transition
         name="fadeInUp"
@@ -201,6 +153,8 @@
       >
         <router-view></router-view>
       </transition>
+
+      <!-- footer -->
       <v-card height="150">
         <v-footer
           absolute
@@ -215,6 +169,7 @@
         </v-footer>
       </v-card>
     </div>
+
     <BackTop></BackTop>
   </v-app>
 </template>
@@ -230,36 +185,12 @@ export default {
     dialog: false,
     drawer: true,
     starDialog: false,
+    mini: false,
     ok: false,
     items: [
-      { icon: "mdi-contacts", text: "Contacts" },
-      { icon: "mdi-history", text: "Frequently contacted" },
-      { icon: "mdi-content-copy", text: "Duplicates" },
-      {
-        icon: "mdi-chevron-up",
-        "icon-alt": "mdi-chevron-down",
-        text: "Labels",
-        model: true,
-        children: [{ icon: "mdi-plus", text: "Create label" }]
-      },
-      {
-        icon: "mdi-chevron-up",
-        "icon-alt": "mdi-chevron-down",
-        text: "More",
-        model: false,
-        children: [
-          { text: "Import" },
-          { text: "Export" },
-          { text: "Print" },
-          { text: "Undo changes" },
-          { text: "Other contacts" }
-        ]
-      },
-      { icon: "mdi-cog", text: "Settings" },
-      { icon: "mdi-message", text: "Send feedback" },
-      { icon: "mdi-help-circle", text: "Help" },
-      { icon: "mdi-cellphone-link", text: "App downloads" },
-      { icon: "mdi-keyboard", text: "Go to the old version" }
+      { name: "1", icon: "mdi-emoticon-outline", text: "开始" },
+      { name: "2", icon: "mdi-eye-plus-outline", text: "发现" },
+      { name: "3", icon: "mdi-account", text: "我的" }
     ]
   }),
   watch: {
@@ -278,6 +209,21 @@ export default {
     });
   },
   methods: {
+    test(index) {
+      switch (index) {
+        case 0:
+          this.$router.push("/home");
+          break;
+        case 1:
+          this.$router.push("/discover");
+          break;
+        case 2:
+          this.$router.push("/my");
+          break;
+        default:
+          break;
+      }
+    },
     star() {
       this.ok = ~this.ok;
       this.starDialog = true;
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,17 +1,100 @@
 <template>
   <v-app>
-    <v-container class="fill-height" fluid>
-      <v-row justify="space-around" align="center">
+    <v-container
+      class="fill-height"
+      fluid
+    >
+      <v-row
+        justify="space-around"
+        align="center"
+      >
         <v-col md="2"></v-col>
-        <v-col md="4">
+        <v-col md="3">
           <v-card>
-            <div class="text-center" margin="0">
-              <video object-fit id="videoCamera" width="100%" height="100%" autoplay></video>
+            <div
+              class="text-center align-center justify-center"
+              margin="0"
+            >
+              <v-card-title class="headline justify-center">
+
+                期待你的面孔
+              </v-card-title>
+              <v-card-text>
+                <video
+                  object-fit
+                  id="videoCamera"
+                  width="95%"
+                  height="95%"
+                  autoplay
+                  v-if="videoOpen"
+                ></video>
+                <v-img
+                  object-fit
+                  width="100%"
+                  height="100%"
+                  contain
+                  :src="imgSrc"
+                  v-if="!videoOpen"
+                ></v-img>
+                <canvas
+                  style="display:none"
+                  object-fit
+                  id="canvasCamera"
+                  :width="videoWidth"
+                  :height="videoHeight"
+                ></canvas>
+
+              </v-card-text>
+
             </div>
+            <v-card-actions
+              d-flex
+              justify="center"
+            >
+              <v-row rows="3">
+                <v-col
+                  cols="12"
+                  md="7"
+                >
+                  <v-file-input
+                    accept="image/*"
+                    color="light-blue darken-3"
+                    small-chips
+                    dense
+                    solo
+                    prepend-icon="mdi-image"
+                    label="File input"
+                  ></v-file-input>
+                </v-col>
+                <v-col md="3">
+                  <!-- </v-btn> -->
+                  <v-btn
+                    dark
+                    color="light-blue darken-3"
+                    @click="videoClick()"
+                  >{{videoInfo}}</v-btn>
+                </v-col>
+                <v-col md="2">
+
+                  <!-- </v-btn> -->
+                  <v-btn
+                    dark
+                    color="light-blue darken-3"
+                    @click="imageClick()"
+                  >{{imageInfo}}</v-btn>
+                </v-col>
+              </v-row>
+
+              <!-- <v-btn
+                dark
+                color="light-blue darken-3"
+              > -->
+
+            </v-card-actions>
           </v-card>
         </v-col>
-        <v-col md="4"
-          ><v-card>
+        <v-col md="4">
+          <v-card>
             <canvas
               style="display:none"
               object-fit
@@ -20,49 +103,77 @@
               :height="videoHeight"
             ></canvas>
             <div class="text-center">
-              <v-img object-fit width="100%" height="100%" contain :src="imgSrc"></v-img>
-            </div> </v-card
-        ></v-col>
-
-        <v-col>
-          <v-btn @click="getCompetence()">打开摄像头</v-btn>
-          <v-btn @click="stopNavigator()">关闭摄像头</v-btn>
-          <v-btn @click="setImage()">拍照</v-btn>
+            </div>
+          </v-card>
         </v-col>
       </v-row>
     </v-container>
   </v-app>
 </template>
 <script>
-import { savePhoto } from '../../api/api.js'
+import { savePhoto } from "../../api/api.js";
+
 export default {
   data() {
     return {
+      absolute: false,
+      finished: false,
+      videoOpen: false,
+      videoInfo: "打开摄像头",
+      imageInfo: "拍照",
+      overlay: false,
       videoWidth: 600,
       videoHeight: 600,
-      imgSrc: '',
+      imgSrc: "https://frozenwhale.oss-cn-beijing.aliyuncs.com/img/man.png",
       thisCancas: null,
       thisContext: null,
-      thisVideo: null,
-    }
+      thisVideo: null
+    };
   },
   mounted() {
-    this.initCanvas()
+    this.initCanvas();
   },
   methods: {
     initCanvas() {
-      console.log('初始化canvas')
-      this.thisCancas = document.getElementById('canvasCamera')
-      this.thisContext = this.thisCancas.getContext('2d')
-      this.drawSmile()
+      console.log("初始化canvas");
+      this.thisCancas = document.getElementById("canvasCamera");
+      this.thisContext = this.thisCancas.getContext("2d");
+      this.drawSmile();
     },
-    // 调用权限（打开摄像头功能）
-    getCompetence() {
-      var _this = this
-      this.thisVideo = document.getElementById('videoCamera')
+    videoClick() {
+      if (this.videoOpen) {
+        this.stopNavigator();
+        this.videoInfo = "打开摄像头";
+        this.videoOpen = false;
+      } else {
+        this.videoOpen = true;
+        this.openVideo();
+
+        this.videoInfo = "关闭摄像头";
+
+        this.imageInfo = "拍照";
+      }
+    },
+    imageClick() {
+      //
+      if (!this.videoOpen) {
+        this.imgSrc =
+          "https://frozenwhale.oss-cn-beijing.aliyuncs.com/img/man.png";
+      } else {
+        this.setImage();
+        this.imageInfo = "清除";
+      }
+      this.stopNavigator();
+      this.videoInfo = "打开摄像头";
+    },
+    openVideo() {
+      var _this = this;
+
+      this.thisVideo = document.getElementById("videoCamera");
+
       // 旧版本浏览器可能根本不支持mediaDevices，我们首先设置一个空对象
       if (navigator.mediaDevices === undefined) {
-        navigator.mediaDevices = {}
+        navigator.mediaDevices = {};
       }
       // 一些浏览器实现了部分mediaDevices，我们不能只分配一个对象
       // 使用getUserMedia，因为它会覆盖现有的属性。
@@ -70,94 +181,106 @@ export default {
       if (navigator.mediaDevices.getUserMedia === undefined) {
         navigator.mediaDevices.getUserMedia = function(constraints) {
           // 首先获取现存的getUserMedia(如果存在)
-          var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.getUserMedia
+          var getUserMedia =
+            navigator.webkitGetUserMedia ||
+            navigator.mozGetUserMedia ||
+            navigator.getUserMedia;
           // 有些浏览器不支持，会返回错误信息
           // 保持接口一致
           if (!getUserMedia) {
-            return Promise.reject(new Error('getUserMedia is not implemented in this browser'))
+            return Promise.reject(
+              new Error("getUserMedia is not implemented in this browser")
+            );
           }
           // 否则，使用Promise将调用包装到旧的navigator.getUserMedia
           return new Promise(function(resolve, reject) {
-            getUserMedia.call(navigator, constraints, resolve, reject)
-          })
-        }
+            getUserMedia.call(navigator, constraints, resolve, reject);
+          });
+        };
       }
       var constraints = {
         audio: false,
         video: {
           width: this.videoWidth,
           height: this.videoHeight,
-          transform: 'scaleX(-1)',
-        },
-      }
+          transform: "scaleX(-1)"
+        }
+      };
       navigator.mediaDevices
         .getUserMedia(constraints)
         .then(function(stream) {
           // 旧的浏览器可能没有srcObject
-          if ('srcObject' in _this.thisVideo) {
-            _this.thisVideo.srcObject = stream
+          if ("srcObject" in _this.thisVideo) {
+            _this.thisVideo.srcObject = stream;
           } else {
             // 避免在新的浏览器中使用它，因为它正在被弃用。
-            _this.thisVideo.src = window.URL.createObjectURL(stream)
+            _this.thisVideo.src = window.URL.createObjectURL(stream);
           }
           _this.thisVideo.onloadedmetadata = function() {
-            _this.thisVideo.play()
-          }
+            _this.thisVideo.play();
+          };
         })
-        .catch((err) => {
-          console.log(err)
-        })
+        .catch(err => {
+          console.log(err);
+        });
     },
+
     //  绘制图片（拍照功能）
 
     setImage() {
-      var _this = this
+      this.finished = true;
+      var _this = this;
       // 点击，canvas画图
-      _this.thisContext.drawImage(_this.thisVideo, 0, 0, _this.videoWidth, _this.videoHeight)
+      _this.thisContext.drawImage(
+        _this.thisVideo,
+        0,
+        0,
+        _this.videoWidth,
+        _this.videoHeight
+      );
       // 获取图片base64链接
-      var image = this.thisCancas.toDataURL('../../../img')
-      console.log(image)
-      console.log('set')
-      _this.imgSrc = image
+      var image = this.thisCancas.toDataURL("../../../img");
+      console.log(image);
+      _this.imgSrc = image;
       // this.drawSmile(_this.thisContext)
       // TODO
-      savePhoto(image.split(',')[1]).then(() => {
-        alert('succ')
-      })
+      savePhoto(image.split(",")[1]).then(() => {});
     },
 
     // base64转文件
 
     dataURLto(dataurl, filename) {
-      var arr = dataurl.split(',')
-      var mime = arr[0].match(/:(.*?);/)[1]
-      var bstr = atob(arr[1])
-      var n = bstr.length
-      var u8arr = new Uint8Array(n)
+      var arr = dataurl.split(",");
+      var mime = arr[0].match(/:(.*?);/)[1];
+      var bstr = atob(arr[1]);
+      var n = bstr.length;
+      var u8arr = new Uint8Array(n);
       while (n--) {
-        u8arr[n] = bstr.charCodeAt(n)
+        u8arr[n] = bstr.charCodeAt(n);
       }
-      return new File([u8arr], filename, { type: mime })
+      return new File([u8arr], filename, { type: mime });
     },
     // 关闭摄像头
 
     stopNavigator() {
-      this.thisVideo.srcObject.getTracks()[0].stop()
+      if (this.thisVideo != null) {
+        this.thisVideo.srcObject.getTracks()[0].stop();
+      }
     },
     drawSmile(ctx) {
-      ctx.beginPath()
-      ctx.arc(75, 75, 50, 0, Math.PI * 2, true) // 绘制
-      ctx.moveTo(110, 75)
-      ctx.arc(75, 75, 35, 0, Math.PI, false) // 口(顺时针)
-      ctx.moveTo(65, 65)
-      ctx.arc(60, 65, 5, 0, Math.PI * 2, true) // 左眼
-      ctx.moveTo(95, 65)
-      ctx.arc(90, 65, 5, 0, Math.PI * 2, true) // 右眼
-      ctx.stroke()
-      console.log(ctx)
-    },
-  },
-}
+      ctx.beginPath();
+      ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // 绘制
+      ctx.moveTo(110, 75);
+      ctx.arc(75, 75, 35, 0, Math.PI, false); // 口(顺时针)
+      ctx.moveTo(65, 65);
+      ctx.arc(60, 65, 5, 0, Math.PI * 2, true); // 左眼
+      ctx.moveTo(95, 65);
+      ctx.arc(90, 65, 5, 0, Math.PI * 2, true); // 右眼
+      ctx.stroke();
+      console.log(ctx);
+    }
+  }
+};
 </script>
 <style lang="scss" scoped>
 .camera_outer {
